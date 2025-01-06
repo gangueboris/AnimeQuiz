089569f870f7d04cd1061195bfc43cce635b6541
gangueboris <gangueboris@gmail.com>
2025-01-06 20:41:59 +0100

    add some feature to quiz component

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -4,12 +4,40 @@ import { Component } from '@angular/core';
   selector: 'app-quiz',
   imports: [],
   template: `
-    <p>
-      quiz works!
-    </p>
+    <section>
+      <div class="quiz__container container">
+        <div class="quiz__header">
+          <div>
+            <h2>AnimeQuiz</h2>
+            <p>{{ nbQuestion }} questions</p>
+          </div>
+          <div><i class="fa-solid fa-stopwatch"></i> <span>00:02:54</span></div>
+        </div>
+
+
+        <div class="questions__container">
+          <div>
+              <div class="questions__container-header">
+                  <p class="number">1</p>
+                  <p>Question ?</p>
+              </div>
+              <div class="questions__container-list">
+                  @for(letter of choicesLetters.slice(0, nbQuestion); track letter) {
+                      <div class="questions__container-content">
+                        <div class="question">{{letter}}. Who is Naruto ?</div>
+                      </div>
+                  }
+              </div>
+              <button class="next-question">Next</button>
+          </div>
+        </div>
+      </div>    
+    </section>
   `,
   styleUrl: './quiz.component.css'
 })
 export class QuizComponent {
+  choicesLetters = ['A', 'B', 'C', 'D'];
+  nbQuestion = 3;
 
 }
