2a7b78e8f608bc16b011d047435008fece3ceaab
gangueboris <gangueboris@gmail.com>
2025-01-08 11:31:01 +0100

    complete complete-quiz component

--- src/app/components/complete-quiz/complete-quiz.component.ts
+++ src/app/components/complete-quiz/complete-quiz.component.ts
@@ -8,17 +8,18 @@ import { Component } from '@angular/core';
         <div class="complete-quiz__container container">
             <i class="fa-solid fa-xmark"></i>
             <div class="complete-quiz-content">
-                <div><img src="" alt="similer"></div>
-                <h3>Your Score</h3>
-                <p>{{ sucessAnswers }} / {{ nbQuestions }}</p>
+                <div class="img__container"><img src="assets/very-happy-emoji.png" alt="similer"></div>
+                <h1>Your Score</h1>
+                <p>{{ sucessAnswers }}/{{ nbQuestions }}</p>
+                <div class="quiz-clock"><i class="fa-solid fa-stopwatch"></i> <p>00:02:54</p></div>
                 <button class="try-again-btn">Try Again</button>
             </div>
             <div class="result__container">
-               <div class="correct-answers">
+               <div class="answers">
                    <i class="fa-regular fa-circle-check"></i>
                    <p>Correct Answers: {{ sucessAnswers }}</p>
                </div>
-               <div class="incorrect-answers">
+               <div class="answers">
                   <i class="fa-regular fa-circle-xmark"></i>
                   <p>Incorrect Answers: {{ nbQuestions - sucessAnswers }}</p>
                </div>
