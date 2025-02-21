0999c38ca196d532bc380fcd48760cba57625b91
gangueboris <gangueboris@gmail.com>
2025-02-21 23:05:10 +0100

    Remove the backEnd form this repository

--- BackEnd/src/main/java/com/github/borisgangue/AnimeQuiz/repository/QuizRepo.java
+++
@@ -1,7 +0,0 @@
-package com.github.borisgangue.AnimeQuiz.repository;
-
-import com.github.borisgangue.AnimeQuiz.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepo extends JpaRepository<Quiz, Long> {
-}
